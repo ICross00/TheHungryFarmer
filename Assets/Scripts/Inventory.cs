fb6031b5dc11c2841d566cd97ee0b6b472af45a7
AUTfks2023 <87923571+AUTfks2023@users.noreply.github.com>
2021-08-31 18:55:42 +1200

    Fixed script error
    Fixed a script error that was appearing in the Inventory script

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -14,7 +14,7 @@ public class Inventory : MonoBehaviour
 
     Item RemoveItem()
     {
-        return;//Must return an item after items have been declared and can be returned.
+        return item[0];//Must return an item after items have been declared and can be returned.
     }
 
     void UpdateInventory()
