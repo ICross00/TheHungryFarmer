51df6c6fc334ac8b86088073f8438ed0cf336cb5
AUTfks2023 <87923571+AUTfks2023@users.noreply.github.com>
2021-08-31 18:37:43 +1200

    First commit
    Testing if commit works

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int gameInt;
+    Inventory inventory;
+    int restaurantRating;
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    int itemID;
+    string name;
+    Sprite sprite;//May need to be changed to srpite 2D in future.
+}
