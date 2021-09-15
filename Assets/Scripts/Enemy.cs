e45a0ba1271f4eac7b39478a72a3e7956f1b2968
AUTfks2023 <87923571+AUTfks2023@users.noreply.github.com>
2021-09-16 01:20:09 +1200

    Player movement
    Created a new script called Mover to allow for enemy movement. It has also replaced most of Player scripts code.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : Fighter
+public class Enemy : Mover
 {
-    private int xpValue;
+    //xpValue determines how much XP is dropped from killing an enemy.
+    public int xpValue = 1;
 }
