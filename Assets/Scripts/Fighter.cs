fd61ad34bf61e8f2eb9fbcf48bd3257d3e8d8950
AUTfks2023 <87923571+AUTfks2023@users.noreply.github.com>
2021-09-16 13:21:49 +1200

    Reding commit
    Everything broke (I hate GitHub) so I had to recreate my branch. The scripts were saved but everything needs to be connected again

--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -4,5 +4,41 @@ using UnityEngine;
 
 public class Fighter : Movable
 {
-    public int hp;
+    //Public fields
+    public int hitPoint = 10;
+    public int maxHitPoint = 10;
+    public float pushRecoverySpeed = 0.2f;
+
+    public int hp;//Still need to find a use for this value.
+
+    protected float immuneTime = 1.0f;
+    protected float lastImmune;
+
+    //Push
+    protected Vector3 pushDirection;
+
+    //Making sure everything classed as a fighter can receive damage and die.
+    protected virtual void ReceiveDamage(Damage dmg)
+    {
+        if (Time.time - lastImmune > immuneTime)
+        {
+            lastImmune = Time.time;
+            hitPoint -= dmg.damageAmount;
+            pushDirection = (transform.position - dmg.origin).normalized * dmg.pushForce;
+
+            //Shows the effect of damage being taken via text on screen.
+            GameManager.instance.ShowText(dmg.damageAmount.ToString(), 25, Color.red, transform.position, Vector3.up * 50, 3.0f);
+
+            if (hitPoint <= 0)
+            {
+                hitPoint = 0;
+                Death();
+            }
+        }
+    }
+
+    protected virtual void Death()
+    {
+
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
     Inventory playerInventory;
 
     int gameInt;
     int restaurantRating;
+
+    //Referencing floating text for later use
+    public FloatingTextManager floatingTextManager;
+
+    //Ability to display floating text after being called with parameters.
+    public void ShowText(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration)
+    {
+        floatingTextManager.Show(msg, fontSize, color, position, motion, duration);
+    }
 }
