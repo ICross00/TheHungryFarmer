8684e6869b9eacdaee01064ad356f70de03c46e8
AUTfks2023 <87923571+AUTfks2023@users.noreply.github.com>
2021-09-18 00:01:39 +1200

    Day/ night cycle
    Added a day and night cycle. The values for how long each is must be altered to make it valid though. Several animation were used on a large rectangle covering the players screen. The opacity will change based on the time.

--- Assets/Scripts/TimeTick.cs
+++ Assets/Scripts/TimeTick.cs
@@ -8,10 +8,24 @@ public class TimeTick : MonoBehaviour
     private int tick;
     private float tickTimer;
     public int maxTicks;
+    public int currentTick;
+    private Animator anim;
 
+    //This will determine where the tick will start before counting up.
+    //This will allow saves of the current tick in future so the game can be started at a specific time.
     private void Awake()
     {
-        tick = 0;
+        tick = currentTick;
+        anim = GetComponent<Animator>();
+
+        if (tick >= 1 && tick <= 15)
+            anim.SetTrigger("IdleDaySelect");
+        else if (tick >= 16 && tick <= 75)
+            anim.SetTrigger("DayToNightSelect");
+        else if (tick >= 76 && tick <= 90)
+            anim.SetTrigger("IdleNightSelect");
+        else if (tick >= 91 && tick <= 150)
+            anim.SetTrigger("NightToDaySelect");
     }
 
     private void Update()
@@ -19,7 +33,7 @@ public class TimeTick : MonoBehaviour
         //Thill will reset the ticks backs to zero after they reach maxTick value.
         if (tick == maxTicks)
         {
-            tick = 0;
+            tick = 1;
         }
 
         //This will update the tick every second (to change time they are updated, update the tickTimer value).
@@ -28,6 +42,16 @@ public class TimeTick : MonoBehaviour
         {
             tickTimer -= tickTimerMax;
             tick++;
+
+
+            //Will determine what time of day/ night it is and play the corresponding animations to change/ stay at said time.
+            if (tick == 10)
+                anim.SetTrigger("DayToNightSelect");
+            else if (tick == 20)
+                anim.SetTrigger("NightToDaySelect");
+
+
+
 
             //This will print the ticks on the player (for testing purposes only, this will be removed).
             GameManager.instance.ShowText("Tick " + tick, 25, Color.red, transform.position, Vector3.up * 50, 3.0f);
