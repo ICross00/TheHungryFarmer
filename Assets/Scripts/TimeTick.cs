622b5ef5457f15d5ab70f388df2a8229c078fb03
AUTfks2023 <87923571+AUTfks2023@users.noreply.github.com>
2021-09-17 21:26:13 +1200

    Tick timer update
    Added comments to the Tick timer script to allow other to understand how it's used a little better.

--- Assets/Scripts/TimeTick.cs
+++ Assets/Scripts/TimeTick.cs
@@ -16,17 +16,20 @@ public class TimeTick : MonoBehaviour
 
     private void Update()
     {
+        //Thill will reset the ticks backs to zero after they reach maxTick value.
         if (tick == maxTicks)
         {
             tick = 0;
         }
 
+        //This will update the tick every second (to change time they are updated, update the tickTimer value).
         tickTimer += Time.deltaTime;
         if(tickTimer > tickTimerMax)
         {
             tickTimer -= tickTimerMax;
             tick++;
 
+            //This will print the ticks on the player (for testing purposes only, this will be removed).
             GameManager.instance.ShowText("Tick " + tick, 25, Color.red, transform.position, Vector3.up * 50, 3.0f);
         }
     }
