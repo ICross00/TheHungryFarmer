25df1002ccd0ec27123f27413cf2bad2323d1c16
samuelbrr9595 <83677150+samuelbrr9595@users.noreply.github.com>
2021-09-23 11:40:24 +1200

    Changed CharcterUI system
    Split the character UI in character menu and HUD

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    //This class contains the methods used in the HUD information
+
+    public Image healthBar;
+
+    float health;
+    float maxHealth;
+    float lerdSpeed;
+
+    private void Start()
+    {
+        health = GameManager.instance.player.maxHitPoint;
+        maxHealth = health;
+    }
+
+    private void Update()
+    {
+        if (health > maxHealth) health = maxHealth;
+
+        lerdSpeed = 3f * Time.deltaTime;
+
+        HealthBarFiller();
+    }
+
+    void HealthBarFiller()
+    {
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, health / maxHealth, lerdSpeed);
+    }
+    
+
+
+}
