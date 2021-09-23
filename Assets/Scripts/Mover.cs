9a8242d38c72aba2065dcf93da72f03f303e2e49
ICross00 <73617207+ICross00@users.noreply.github.com>
2021-09-23 13:35:43 +1200

    Revert "Revert "Revert "Revert "Merge branch 'main' into Ishaiahs-Branch""""
    This reverts commit 45c06588b254b84bab16ff68619687616d1c3c01.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,59 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Mover : Fighter
+public class Mover : Fighter
 {
-    public int experience;
-    public int energy;
-    
-    protected Vector3 moverLocation;
-    protected BoxCollider2D boxCollider;
-    protected RaycastHit2D hit;
-    //Controls movement speed.
-    public float ySpeed = 4f;
-    public float xSpeed = 5f;
+    private Rigidbody2D rb2d;
 
+    //Controls the Speed of the Object
+    public float xSpeed = 1f;
+    public float ySpeed = 1f;
+    
     protected virtual void Start()
     {
-        boxCollider = GetComponent<BoxCollider2D>();
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        this.rb2d.gravityScale = 0.0f;
+        this.rb2d.velocity = Vector2.zero;
     }
 
-    //Handle 2D trigger enter events in this function inside derived classes
-    protected virtual void OnTriggerEnter2D(Collider2D collider) { }
-
-    //Handle 2D trigger exit events in this function inside derived classes
-    protected virtual void OnTriggerExit2D(Collider2D collider) { }
-
-    protected virtual void UpdateMotor(Vector3 input)
+    //Must be Overridden in the child class FixedUpdate method
+    protected virtual void UpdateMotor(Vector2 input)
     {
-        //Reset movedealta.
-        moverLocation = new Vector3(input.x * xSpeed, input.y * ySpeed, 0);
-
-        //Changing direction sprite is facing (left or right)
-        if (moverLocation.x > 0)
-            transform.localScale = Vector3.one;
-        else if (moverLocation.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
-
-        //Add push vector if there are any.
-        moverLocation += pushDirection;
-
-        //Reduce push force every frame to prevent what has been pushed from going on FOREVER AND EVER AND EVER AN.....
-        pushDirection = Vector3.Lerp(pushDirection, Vector3.zero, pushRecoverySpeed);
-
-        //Checking if a box is in the direction the payer is moving. If a box is there, the mover will not be allowed to move there.
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moverLocation.y), Mathf.Abs(moverLocation.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)
+        if (input.x > 0 || input.x < 0)
         {
-            //Allowing the sprite to move
-            transform.Translate(0, moverLocation.y * Time.deltaTime, 0);
+            rb2d.AddForce(new Vector2(input.x * xSpeed, 0), ForceMode2D.Impulse);
         }
 
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moverLocation.x, 0), Mathf.Abs(moverLocation.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)
+        if (input.y > 0 || input.y < 0)
         {
-            //Allowing the sprite to move
-            transform.Translate(moverLocation.x * Time.deltaTime, 0, 0);
+            rb2d.AddForce(new Vector2(0, input.y * ySpeed), ForceMode2D.Impulse);
         }
     }
 }
