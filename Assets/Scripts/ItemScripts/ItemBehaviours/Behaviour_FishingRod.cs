e4bd391c940d086629037cde7c39837228af7674
dvtuanann <vtuananh.d@gmail.com>
2021-10-20 14:17:57 +1300

    Delete file not needed

--- Assets/Scripts/ItemScripts/ItemBehaviours/Behaviour_FishingRod.cs
+++ Assets/Scripts/ItemScripts/ItemBehaviours/Behaviour_FishingRod.cs
@@ -10,16 +10,16 @@ public class Behaviour_FishingRod : ItemBehaviour
 
     public override void OnItemEquipped(Item item, Player player, Inventory inventory) {
         //fishingRod
-        GameObject rodPrefab = Resources.Load<GameObject>("Prefabs/FishingRod"); //TODO: Get the prefab name from a tag value of the sword item
-        GameObject rod = GameObject.Instantiate(rodPrefab, player.transform.position, Quaternion.identity, player.transform);
-        rod.name = "Equipped Fishing Rod";
+        GameObject frodPrefab = Resources.Load<GameObject>("Prefabs/FishingRod"); //TODO: Get the prefab name from a tag value of the sword item
+        GameObject frod = GameObject.Instantiate(frodPrefab, player.transform.position, Quaternion.identity, player.transform);
+        frod.name = "Equipped Fishing Rod";
     }
 
     public override void OnItemUnequipped(Item item, Player player, Inventory inventory) 
     {
-        Transform rodInstance = player.transform.Find("Equipped Fishing Rod");
-        if(rodInstance != null)
-            Object.Destroy(rodInstance.gameObject);
+        Transform frodInstance = player.transform.Find("Equipped Fishing Rod");
+        if(frodInstance != null)
+            Object.Destroy(frodInstance.gameObject);
     }
 
 }
