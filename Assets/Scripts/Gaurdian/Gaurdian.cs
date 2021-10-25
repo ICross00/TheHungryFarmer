d2b675c04cf098d063c5f6d854ea52e4b5d0abe6
AUTfks2023 <87923571+AUTfks2023@users.noreply.github.com>
2021-10-25 13:53:39 +1300

    Revert "Gaurdian Health fix"
    This reverts commit 076a873b9516ac5b77e0171cbdf22fdd8a497e41.

--- Assets/Scripts/Gaurdian/Gaurdian.cs
+++ Assets/Scripts/Gaurdian/Gaurdian.cs
@@ -6,7 +6,6 @@ public class Gaurdian : MonoBehaviour
 {
     //Public fields
     public int hitPoint = 150;
-    public int maxHitPoint;
     public float pushRecoverySpeed = 0.2f;
     Throwable knife;
     Pike pike1;
@@ -62,13 +61,13 @@ public class Gaurdian : MonoBehaviour
 
     private void Update()
     {
-        if (hitPoint > (maxHitPoint / 2))
+        if (hitPoint > (hitPoint / 2))
         {
             anim.SetBool("Full", true);
             anim.SetBool("Half", false);
             anim.SetBool("None", false);
         }
-        else if (hitPoint > 0 && hitPoint < ((maxHitPoint / 2) + 1))
+        else if (hitPoint > 0 && hitPoint < ((hitPoint / 2) + 1))
         {
             anim.SetBool("Full", false);
             anim.SetBool("Half", true);
@@ -161,14 +160,14 @@ public class Gaurdian : MonoBehaviour
     {
         lootMuliplier = GameObject.Find("ThrowingWeapon").GetComponent<ThrowingWeapon>().lootDropChance;
 
-        if (gaurdianName == "gaurdianOne")
+        if (gaurdianName == "")
         {
             //TODO: Add loot to the first gaurdian as seen below
 
             //Collectable.Spawn(transform.position, "Gaurdianloot", 1 * lootMuliplier, 1.0f);
             //Collectable.Spawn(transform.position, "GoldCoin", Random.Range(5, 10) * lootMuliplier, 1.0f);
         }
-        else if (gaurdianName == "GaurdianTwo")
+        else if (gaurdianName == "")
         {
             //TODO: Add loot to the second gaurdian as seen below
 
