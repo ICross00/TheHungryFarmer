683c3e005d680faefa3d419da11949ab6b0122a0
AUTfks2023 <87923571+AUTfks2023@users.noreply.github.com>
2021-10-25 14:11:40 +1300

    GitHubs f**kup fix
    The .meta file for atlas has bee replaced entirely. I will have to redo the Pig and Rabbit animations again (THANKS GITHUB, YOU D**K)

--- Assets/Windmill.cs
+++ Assets/Windmill.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class Windmill : MonoBehaviour
 {
-<<<<<<< HEAD
     public float rotationSpeed = 15.0f;
-=======
-    public float rotationSpeed = 10.0f;
->>>>>>> parent of aed4538 (Revert "Merge branch 'main' into Liams_branch_take3")
     public Transform bladesTransform;
 
     void Update()
