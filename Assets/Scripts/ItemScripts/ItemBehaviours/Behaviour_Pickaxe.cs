1a93b2555c8ab1a79f5421d3b4515ff26b089078
ICross00 <73617207+ICross00@users.noreply.github.com>
2021-10-26 23:40:25 +1300

    Deleted test scenes

--- Assets/Scripts/ItemScripts/ItemBehaviours/Behaviour_Pickaxe.cs
+++ Assets/Scripts/ItemScripts/ItemBehaviours/Behaviour_Pickaxe.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Behaviour_Pickaxe : ItemBehaviour
 {
     private const float PICKAXE_RANGE = 1.25f;
-
+    private IEnumerator cr;
     public override void OnItemUsed(Item item, Player player, Inventory inventory) {
         MonoBehaviour plyAsMono = (MonoBehaviour)player; //Start a coroutine on the player's monobehaviour
-        plyAsMono.StartCoroutine(SetMiningAnimation(player));
+        cr = SetMiningAnimation(player);
+        plyAsMono.StartCoroutine(cr);
     }
 
     public override void OnItemEquipped(Item item, Player player, Inventory inventory) {
@@ -16,6 +17,8 @@ public class Behaviour_Pickaxe : ItemBehaviour
     }
 
     public override void OnItemUnequipped(Item item, Player player, Inventory inventory) {
+        Animator animator = player.GetComponent<Animator>();
+        animator.SetBool("isMining", false);
 
     }
 
@@ -24,13 +27,14 @@ public class Behaviour_Pickaxe : ItemBehaviour
     //Direction: 0 = left, 1 = up, 2 = right, 3 = down
     */
     public void OnMine(Player player, int direction) {
+        Debug.Log("OnMine");
         //Calculate angle to ore
         float angle = direction * Mathf.PI / 2.0f;
         Vector3 offset = new Vector3(-Mathf.Cos(angle) * PICKAXE_RANGE, Mathf.Sin(angle) * PICKAXE_RANGE - 0.75f, 0);
 
         //Find ore in front of player
         MinableObject targetObj = MinableObject.GetObjectAtPosition(player.transform.position + offset);
-        Debug.Log(targetObj);
+
         if(targetObj != null)
             targetObj.Mine();
     }
